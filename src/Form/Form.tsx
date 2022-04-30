832edfb51221900735ea78840363e5ba0defaa84
Hencky <928856778@qq.com>
2022-04-30 15:24:00 +0800

    feat: 表单组件

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,3 +1,17 @@
 import { Form as AForm } from 'antd';
+import { FormGroup } from './FormGroup';
+import { FormItem } from './FormItem';
 
-export { AForm as Form };
+type IFormType = typeof AForm;
+
+interface FormType extends IFormType {
+  FormGroup: typeof FormGroup;
+  FormItem: typeof FormItem;
+}
+
+const Form = AForm as FormType;
+
+Form.FormGroup = FormGroup;
+Form.FormItem = FormItem;
+
+export { Form };
--- src/Form/index.ts
+++ src/Form/index.ts
@@ -1,3 +1,4 @@
 export * from './FormGroup';
 export * from './FormItem';
 export * from './Form';
+export * from './interface';
