8ca27ea861ba5a2cfe275dd980d7c072df65db56
Hencky <928856778@qq.com>
2022-05-17 09:23:12 +0800

    docs: 其他录入组件展示文档

---
+++ src/entrys/README.md
@@ -0,0 +1,12 @@
+---
+title: Entry
+subGroup: 录入组件
+---
+
+# 录入组件预览
+
+## Select
+
+支持`FormItem`的`dataSource`和`remoteDataSource`属性的选择器组件。
+
+<Demo src="./Select/demos/select.tsx" />
---
+++ src/entrys/Select/demos/select.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Select } from '@pms/ui';
+
+const Demo = () => {
+  const onChange = (value) => {
+    console.log('value', value);
+  };
+
+  return (
+    <Select
+      style={{ width: 200 }}
+      dataSource={[
+        { label: '选项1', value: '1' },
+        { label: '选项2', value: '2' },
+      ]}
+      placeholder="请选择"
+      onChange={onChange}
+    />
+  );
+};
+
+export default Demo;
