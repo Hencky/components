86a88a16119a5fed0c2027ba879196a3366bda86
Hencky <928856778@qq.com>
2024-03-28 09:47:35 +0800

    fix: 修复表单name不填报错问题

--- src/_util/formKey.ts
+++ src/_util/formKey.ts
@@ -3,7 +3,9 @@ import type { FormItemProps } from '../FormItem';
 
 export function getFormKey(field: FormItemProps): string;
 export function getFormKey(name: FormItemProps['name']): string;
-export function getFormKey(name: any): string {
+export function getFormKey(name: any): any {
+  if (!name) return;
+
   if (typeof name === 'string' || Array.isArray(name)) {
     return toArray(name).join('-');
   }
