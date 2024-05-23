a633994446066ee0957da581f621100ec3278188
Hencky <928856778@qq.com>
2024-05-23 10:44:34 +0800

    feat: 修正样式方案

--- src/_hooks/usePrefix.ts
+++ src/_hooks/usePrefix.ts
@@ -1,12 +1,17 @@
-import { useContext } from 'react';
-import { ConfigProvider } from 'antd';
+// import { useContext } from 'react';
+// import { ConfigProvider } from 'antd';
 
-export const usePrefix = (
-  tag?: string,
-  props?: {
-    prefixCls?: string;
-  }
-) => {
-  const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
-  return getPrefixCls(tag, props?.prefixCls);
+// export const usePrefix = (
+//   tag?: string,
+//   props?: {
+//     prefixCls?: string;
+//   }
+// ) => {
+//   const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
+//   // return getPrefixCls(tag, props?.prefixCls);
+//   return 'lucky-bird-ui-' + tag;
+// };
+
+export const usePrefix = (tag?: string) => {
+  return 'lucky-bird-ui-' + tag;
 };
